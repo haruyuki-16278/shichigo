93b5766b5318d2b83f834c88d9f1075e4a8b2503
haruyuki@gemibook-win <haruyuki.program@gmail.com>
2023-09-26 08:34:21 +0900

    👍 templatesとsqliteを追加

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+import sqlite3
+
+dbname = 'shichigo.db'
+conx = sqlite3.connect(dbname)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# ページルーティング
+@app.route('/')
+def redirect_index():
+    return redirect(url_for('compose'))
+
+@app.route('/compose')
+def compose():
+    return render_template('compose.jinja.html')
+
+# APIルーティング
+@app.route('/shichigo', methods=['GET', 'POST'])
+def shichigo():
+    # if request.method == 'POST':
+        # cur = conx.cursor()
+        # cur.execute('INSERT INTO shichigo PARAM')
+    return ''
